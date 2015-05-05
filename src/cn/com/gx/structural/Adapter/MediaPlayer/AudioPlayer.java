63e9ccf7b2f4dd8c8273a4636cf5c6ac26ab3bd1
gxbaggio <350769410@qq.com>
2015-05-05 14:59:51 +0800

    适配器模式
    在适配器模式中引入了一个被称为适配器（Adapter，即MediaAdapter）的包装类，而它所包装的对象称为适配者（Adaptee，即AdvancedMediaPlayer），即被适配的类。适配器的实现就是客户类（即AudioPlayer）的请求转化为对适配者的相应接口的调用。也就是说：当客户类调用适配器的方法时，在适配器类的内部将调用适配者类的方法，而这个过程对客户类是透明的，客户类并不直接访问适配者类。因此，适配器让那些由于接口不兼容而不能交互的类可以一起工作。
    
    适配器模式可以将一个类的接口和另一个类的接口匹配起来，而无须修改原来的适配者接口（AdvancedMediaPlayer）和抽象目标类接口（MediaPlayer）。

--- src/cn/com/gx/structural/Adapter/MediaPlayer/AudioPlayer.java
+++ src/cn/com/gx/structural/Adapter/MediaPlayer/AudioPlayer.java
@@ -13,7 +13,7 @@ public class AudioPlayer implements MediaPlayer
 			mediaAdapter = new MediaAdapter(audioType);
 			mediaAdapter.play(audioType, fileName);
 		}else{
-			System.out.println("Invalid meida. " + audioType + " format not supported");
+			System.out.println("Invalid media. " + audioType + " format not supported");
 		}
 	}
 
