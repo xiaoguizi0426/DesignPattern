a4bba356ebe405754176b0c88eb56e99305bd3fd
gxbaggio <350769410@qq.com>
2015-05-05 16:33:55 +0800

    单例模式
    确保某一个类只有一个实例，而且自行实例化并向整个系统提供这个实例，这个类称为单例类，它提供全局访问的方法。

--- src/cn/com/gx/creational/Singleton/SingleObject.java
+++ src/cn/com/gx/creational/Singleton/SingleObject.java
@@ -2,11 +2,14 @@ package cn.com.gx.creational.Singleton;
 
 public class SingleObject
 {
-	private static SingleObject instance = new SingleObject();
+	private static SingleObject instance = null;
 	
 	private SingleObject(){}
 	
 	public static SingleObject getInstance(){
+		if(instance == null){
+			instance = new SingleObject();
+		}
 		return instance;
 	}
 	
