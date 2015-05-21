c61ff6c2a809067fb55a42e9b2e964d52c9c5dab
gxbaggio <350769410@qq.com>
2015-05-21 17:01:31 +0800

    备忘录模式
    在不破坏封装的前提下，捕获一个对象的内部状态，并在该对象之外保存这个状态，这样可以在以后将对象恢复到原先保存的状态。

---
+++ src/cn/com/gx/behavioral/Memento/Memento.java
@@ -0,0 +1,14 @@
+package cn.com.gx.behavioral.Memento;
+
+public class Memento
+{
+	private String state;
+	
+	public Memento(String state){
+		this.state = state;
+	}
+	
+	public String getState(){
+		return state;
+	}
+}
